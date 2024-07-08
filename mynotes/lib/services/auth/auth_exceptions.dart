c799f6422498617b3e8112596682cf358ccd1faf
Mr Crazy <you@example.com>
2024-07-08 08:56:11 -0700

    step-8

---
+++ mynotes/lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,14 @@
+// login exceptions
+class UserNotFoundAuthException implements Exception {}
+
+class WrongPasswordAuthException implements Exception {}
+
+class InvalidCreadentialAuthException implements Exception {}
+
+// register exceptions
+
+class WeakPasswordAuthException implements Exception {}
+
+class EmailAlredyInUseAuthException implements Exception {}
+
+class EmailAlredyInUseAuthException implements Exception {}
